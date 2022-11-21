c8cc5df6c77406bd852e12788651ce3dc0b296a6
Nelly <$>
2022-11-20 23:06:32 -0500

    now able to mark task as complete

--- app.js
+++ app.js
@@ -1,58 +1,64 @@
-// This is the array that will hold the todo list items
 let todoItems = [];
 
 function renderTodo(todo) {
-    // Select the first element with a class of `js-todo-list`
-    const list = document.querySelector('.js-todo-list');
-  
-    // Use the ternary operator to check if `todo.checked` is true
-    // if so, assign 'done' to `isChecked`. Otherwise, assign an empty string
-    const isChecked = todo.checked ? 'done': '';
-    // Create an `li` element and assign it to `node`
-    const node = document.createElement("li");
-    // Set the class attribute
-    node.setAttribute('class', `todo-item ${isChecked}`);
-    // Set the data-key attribute to the id of the todo
-    node.setAttribute('data-key', todo.id);
-    // Set the contents of the `li` element created above
-    node.innerHTML = `
-      <input id="${todo.id}" type="checkbox"/>
-      <label for="${todo.id}" class="tick js-tick"></label>
-      <span>${todo.text}</span>
-      <button class="delete-todo js-delete-todo">
-      <svg><use href="#delete-icon"></use></svg>
-      </button>
-    `;
-  
-    // Append the element to the DOM as the last child of
-    // the element referenced by the `list` variable
+  const list = document.querySelector('.js-todo-list');
+  const item = document.querySelector(`[data-key='${todo.id}']`);
+
+  const isChecked = todo.checked ? 'done': '';
+  const node = document.createElement("li");
+  node.setAttribute('class', `todo-item ${isChecked}`);
+  node.setAttribute('data-key', todo.id);
+  node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `;
+
+  if (item) {
+    list.replaceChild(node, item);
+  } else {
     list.append(node);
   }
-  function addTodo(text) {
-    const todo = {
-      text,
-      checked: false,
-      id: Date.now(),
-    };
-  
-    todoItems.push(todo);
-    renderTodo(todo);
-  }
+}
 
-// Select the form element
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  renderTodo(todo);
+}
+
+function toggleDone(key) {
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
+}
+
+
 const form = document.querySelector('.js-form');
-// Add a submit event listener
 form.addEventListener('submit', event => {
-  // prevent page refresh on form submission
   event.preventDefault();
-  // select the text input
   const input = document.querySelector('.js-todo-input');
 
-  // Get the value of the input and remove whitespace
   const text = input.value.trim();
   if (text !== '') {
     addTodo(text);
     input.value = '';
     input.focus();
+  }
+});
+
+const list = document.querySelector('.js-todo-list');
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('js-tick')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
   }
 });
